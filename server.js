a56f51a5e92a438502bf5cdea7efc4c487e9622a
Lê Thiện <160819333+Ku-Shin-24@users.noreply.github.com>
2024-12-11 16:42:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1290,7 +1290,7 @@ app.post('/login', async (req, res) => {
         // 5. Lưu token vào cookie
         res.cookie('token', token, {
             httpOnly: false, // Cookie không thể truy cập bằng JavaScript
-            secure: false,  // Chỉ đặt thành true khi bạn dùng HTTPS (cần thiết cho production)
+            secure: process.env.NODE_ENV === 'production',  // Chỉ đặt thành true khi bạn dùng HTTPS (cần thiết cho production)
             sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // 'None' cho production, 'Lax' cho development
             maxAge: 3600000, // 1 giờ (đơn vị là mili giây)
             domain: process.env.NODE_ENV === 'production'? 't-sport-api.onrender.com' : undefined 
